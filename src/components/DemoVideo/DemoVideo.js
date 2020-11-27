53aaeee7977fef23c951bc9fabc1d40ea212725b
Mikail <mikailrahman1@hotmail.com>
2020-11-27 10:53:26 -0500

    updated demo video

--- src/components/DemoVideo/DemoVideo.js
+++ src/components/DemoVideo/DemoVideo.js
@@ -8,7 +8,7 @@ export default function DemoVideo() {
         // width="560"
         // height="315"
         className="demo-video"
-        src="https://www.youtube.com/embed/rvLrWYl8N-o"
+        src="https://www.youtube.com/embed/tTSTFI_cTw4"
         frameborder="0"
         allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
