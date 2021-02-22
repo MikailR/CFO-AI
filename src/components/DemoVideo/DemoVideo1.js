5353597df1e62b540996a8a857e56e7018e2622f
Mikail <mikailrahman1@hotmail.com>
2021-02-22 17:08:34 -0500

    Updated video

--- src/components/DemoVideo/DemoVideo1.js
+++ src/components/DemoVideo/DemoVideo1.js
@@ -1,13 +1,10 @@
 import React from "react";
-import ReactPlayer from "react-player";
 import "./DemoVideo.css";
-
-import logo from "../../assets/thumbnail/thumbnail.png";
 
 export default function DemoVideo() {
   return (
-    <div className="demo-video">
-      {/* <iframe
+    <div className="demo-video-container flex-center-row">
+      <iframe
         // width="560"
         // height="315"
         className="demo-video"
@@ -16,17 +13,7 @@ export default function DemoVideo() {
         frameborder="0"
         allow="accelerometer; fullscreen; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
-      ></iframe> */}
-
-      <ReactPlayer
-        className="demo-video-player"
-        height="100%"
-        width="100%"
-        controls
-        playing
-        light={logo}
-        url="https://elite-static.oss-cn-shenzhen.aliyuncs.com/CFO%20AI%20Demo%20Video.mp4"
-      />
+      ></iframe>
     </div>
   );
 }
